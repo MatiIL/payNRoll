637b0c2ac44841614142b96c05007542df13f57e
MatiIL <matibres@gmail.com>
2023-08-02 13:47:13 +0300

    login & signup forms functional

--- FE/src/app/auth/auth-form/auth-form/auth-form.component.ts
+++ FE/src/app/auth/auth-form/auth-form/auth-form.component.ts
@@ -1,8 +1,15 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-auth-form',
@@ -10,22 +17,50 @@ import {MatFormFieldModule} from '@angular/material/form-field';
   styleUrls: ['./auth-form.component.scss'],
   encapsulation: ViewEncapsulation.None,
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, NgIf],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+  ],
 })
 export class AuthFormComponent {
   @Input() noAccount: boolean = false;
-  
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-  firstName = new FormControl('', [Validators.required]);
-  lastName = new FormControl('', [Validators.required]);
-  teamName = new FormControl('');
 
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    teamName: new FormControl(''),
+  });
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  getErrorMessage(control: AbstractControl | null) {
+    if (control && control.hasError('required')) {
       return 'נא להכניס כתובת מייל תקינה!';
     }
 
-    return this.email.hasError('email') ? 'נא להכניס כתובת מייל תקינה!' : '';
+    return control && control.hasError('email')
+      ? 'נא להכניס כתובת מייל תקינה!'
+      : '';
+  }
+
+  onSubmit(form: FormGroup) {
+    if (this.noAccount) {
+      //code for signup
+      console.log(form.value);
+    } else {
+      console.log(form.value);
+      form.reset();
+    }
   }
 }
