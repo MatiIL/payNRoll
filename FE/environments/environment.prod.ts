9e0e4556ad85fb02a23280778a7fb14baab8c72f
MatiIL <matibres@gmail.com>
2023-08-19 14:36:26 +0300

    fixed env.prod.ts

--- FE/environments/environment.prod.ts
+++ FE/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiKey: process.env.API_KEY || 'default-api-key',
-    signupCode: process.env.SIGNUP_CODE || 'default-signup-code'
+    apiKey: process.env['API_KEY'] || 'default-api-key',
+    signupCode: process.env['SIGNUP_CODE'] || 'default-signup-code'
   };
   
