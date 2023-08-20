dea62ad6a74286046899ce555551ccbf3ea781d6
MatiIL <matibres@gmail.com>
2023-08-20 12:38:34 +0300

    changed environment imports

--- FE/src/app/auth/auth.service.ts
+++ FE/src/app/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginInput, User } from '../../generated-types';
 import { Observable, BehaviorSubject } from 'rxjs';
-import { environment } from '../../environment/environment.prod';
+import { environment } from '../../environment/environment';
 
 const getServerUrl = () => {
   if (environment.production) {
