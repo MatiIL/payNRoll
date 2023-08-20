d9c47a271463709dd20a9c68d8bf3d7bb21cd5cb
MatiIL <matibres@gmail.com>
2023-08-20 13:34:39 +0300

    fixed env.prod

--- FE/src/environment/environment.prod.ts
+++ FE/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  signupCode: process.env.SIGNUP_CODE || 'default-signup-code',
+  signupCode: process.env['SIGNUP_CODE'] || 'default-signup-code',
   openAiKey: process.env['OPENAI_KEY'] || 'default-openai-key',
 };
