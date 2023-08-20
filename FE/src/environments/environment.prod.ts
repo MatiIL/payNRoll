f86b72e0c5e170631c5d4c84e63401f1f25a5eba
MatiIL <matibres@gmail.com>
2023-08-20 18:12:23 +0300

    updated all .gitignores

--- FE/src/environments/environment.prod.ts
+++ FE/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   signupCode: 'payNroll2023/24!',
-  openAiKey: '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',
+  openAiKey: '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',
 };
