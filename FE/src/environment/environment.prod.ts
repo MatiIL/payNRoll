f8da0bd1bc16572123ee4f16596b02d7b1f9bcc9
MatiIL <matibres@gmail.com>
2023-08-20 12:55:05 +0300

    updated openAiKey

--- FE/src/environment/environment.prod.ts
+++ FE/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     signupCode: 'payNroll2023/24!',
-    openAiKey: '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',
+    openAiKey: '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',
   };
   
