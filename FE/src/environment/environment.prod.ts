b5c6f321a792fce3d48f8249c03230b04da404c1
MatiIL <matibres@gmail.com>
2023-08-20 12:31:36 +0300

    removed process.env

--- FE/src/environment/environment.prod.ts
+++ FE/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    signupCode: process.env['SIGNUP_CODE'] || 'default-signup-code',
-    openAiKey: process.env['OPENAI_KEY'] || 'default-openai-key',
+    signupCode: 'default-signup-code',
+    openAiKey: 'default-openai-key',
   };
   
