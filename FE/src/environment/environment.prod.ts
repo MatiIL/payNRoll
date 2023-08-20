bcc44501b8a130f8de885c25463c3fbe84da769e
MatiIL <matibres@gmail.com>
2023-08-20 13:06:25 +0300

    protected env values

--- FE/src/environment/environment.prod.ts
+++ FE/src/environment/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: true,
-    signupCode: 'payNroll2023/24!',
-    openAiKey: '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',
-  };
-  
+  production: true,
+  signupCode: 'default-signup-code',
+  openAiKey: 'default-openai-key',
+};
