37cfb1896ef63db2e370bfacfcfce0b243cbe36c
MatiIL <matibres@gmail.com>
2023-08-20 23:22:41 +0300

    added server url to codegen.yaml & graphql.module

--- FE/src/app/graphql.module.ts
+++ FE/src/app/graphql.module.ts
@@ -2,17 +2,15 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-import { getServerUrl } from './utils';
 
+const uri = 'https://paynroll-server.onrender.com/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({
-      uri: `${getServerUrl}/graphql`,
-      withCredentials: true, 
-    }),
+    link: httpLink.create({ uri }),
     cache: new InMemoryCache(),
   };
 }
+
 
 @NgModule({
   exports: [ApolloModule],
