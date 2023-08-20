df39c18731a7f4a431ffccc37381891e168c7e01
MatiIL <matibres@gmail.com>
2023-08-20 18:17:36 +0300

    test

--- FE/src/environments/environment.prod.ts
+++ FE/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   signupCode: 'payNroll2023/24!',
-  openAiKey: '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',
+  openAiKey: 'shame_key',
 };
