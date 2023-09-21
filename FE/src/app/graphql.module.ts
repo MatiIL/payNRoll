14b54ecaec19904dda6c2fcf8fdae0a8c7042a0c
MatiIL <matibres@gmail.com>
2023-09-21 14:35:46 +0300

    fixed URI

--- FE/src/app/graphql.module.ts
+++ FE/src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'http://paynroll-server.onrender.com/graphql'; 
+const uri = 'https://paynroll-server.onrender.com/graphql'; 
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
