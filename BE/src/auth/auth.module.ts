4a06e82f93f6499a7b10b4744513a2816bf4ab68
MatiIL <matibres@gmail.com>
2024-02-01 12:08:39 +0200

    fixed import to AuthModule

--- BE/src/auth/auth.module.ts
+++ BE/src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from '../users/users.module';
@@ -6,10 +7,14 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
+import { YahooAuthService } from './yahoo/yahoo-auth.service';
+import { OAuthService } from './o-auth/oauth-service';
 import { YahooStrategy } from './strategies/yahoo.strategy';
+import { YahooAuthGuard } from './guards/yahoo-auth.guard';
 
 @Module({
   imports: [
+    PassportModule,
     UserModule,
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
@@ -22,6 +27,6 @@ import { YahooStrategy } from './strategies/yahoo.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy, JwtStrategy, YahooStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy, YahooAuthService, OAuthService, YahooStrategy, YahooAuthGuard],
 })
 export class AuthModule {}
