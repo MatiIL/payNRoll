0b6072b87ac7362f8f37c38177811460a5fb3305
MatiIL <matibres@gmail.com>
2024-02-01 16:02:28 +0200

    added access_token to headers

--- FE/src/app/auth/auth.service.ts
+++ FE/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginInput, User } from '../../generated-types';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
@@ -20,15 +20,30 @@ export class AuthService {
       return this.loggedInSubject.asObservable();
     }
 
-  login(loginRequest: LoginInput): Observable<any> {
-    const url = `${this.apiUrl}/login`; 
-    return this.httpClient.post<User>(url, loginRequest, { observe: 'response' });
-  }
-
-  yahooAuthenticate(): Observable<any> {
-    const url = `${this.apiUrl}/yahoo-authenticate`;
-    return this.httpClient.get(url, { observe: 'response' });
-  }
+    login(loginRequest: LoginInput): Observable<any> {
+      const url = `${this.apiUrl}/login`;
+      return this.httpClient.post<User>(url, loginRequest, { observe: 'response' }).pipe(
+        tap((response) => {
+          const accessToken = response.headers.get('Authorization');
+          if (accessToken) {
+            // Store the access token in a secure way (e.g., local storage, state management)
+            // You may also need to handle token expiration and refreshing
+            localStorage.setItem('access_token', accessToken);
+          }
+        })
+      );
+    }
+  
+    yahooAuthenticate(): Observable<any> {
+      const url = `${this.apiUrl}/yahoo-authenticate`;
+  
+      // Include the access token in the headers
+      const headers = new HttpHeaders({
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+      });
+  
+      return this.httpClient.get(url, { headers, observe: 'response' });
+    }
 
   logout() {
     const url = `${this.apiUrl}/logout`; 
