5d479fdaab7ccce3f85a190562140c6c8c11eb8a
MatiIL <matibres@gmail.com>
2024-08-27 01:04:49 +0300

    updated rookie salary scale table

--- FE/src/app/rook-salary-scale/rook-salary-scale.component.ts
+++ FE/src/app/rook-salary-scale/rook-salary-scale.component.ts
@@ -10,18 +10,14 @@ import { MatTableModule } from '@angular/material/table';
   imports: [CommonModule, MatTableModule]
 })
 export class RookSalaryScaleComponent {
+  rookieSalaries: Array<number> = [4, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1]
+  sophmoreSalaries: Array<number> = [8, 6, 4, 3, 3, 2, 2, 2, 1, 1, 1];
+  fourthYearSalary: number = 0;
 
-  sophmoreSalaries: Array<number> = [5, 4, 4, 3, 3, 2, 2, 2, 1, 1, 1];
-  fifthYearSalary: number = 0;
-
-  calculateExtension(sophmoreSalary: number) {
+  calculateExtension(rookieSalary: number, sophmoreSalary: number) {
     let extensionSalaries: Array<number> = [];
-    if (sophmoreSalary * 4 + sophmoreSalary < 10) {
-      this.fifthYearSalary = 10;
-    } else {
-      this.fifthYearSalary = sophmoreSalary * 4 + sophmoreSalary
-    }
-    extensionSalaries = [this.fifthYearSalary, this.fifthYearSalary + sophmoreSalary, this.fifthYearSalary + 2 * sophmoreSalary];
+    this.fourthYearSalary = 8 + sophmoreSalary + rookieSalary;
+    extensionSalaries = [this.fourthYearSalary, this.fourthYearSalary + sophmoreSalary, this.fourthYearSalary + 2 * sophmoreSalary, this.fourthYearSalary + 3 * sophmoreSalary];
 
     return extensionSalaries;
   }
@@ -29,10 +25,10 @@ export class RookSalaryScaleComponent {
 
   dataSource = Array.from({ length: 11 }, (_, index) => ({
     column1: `${index + 1}`,
-    column2: `${this.sophmoreSalaries[index]}$`,
-    column3: `${this.sophmoreSalaries[index] * 2}$`,
-    column4: `${this.sophmoreSalaries[index] * 4}$`,
-    extendedData: this.calculateExtension(this.sophmoreSalaries[index]),
+    column2: `${this.rookieSalaries[index]}$`,
+    column3: `${this.sophmoreSalaries[index]}$`,
+    column4: `${this.sophmoreSalaries[index] * 2}$`,
+    extendedData: this.calculateExtension(this.rookieSalaries[index], this.sophmoreSalaries[index]),
   }));
 
   displayedColumns: string[] = ['column1', 'column2', 'column3', 'column4'];
@@ -49,6 +45,7 @@ export class RookSalaryScaleComponent {
         'column5',
         'column6',
         'column7',
+        'column8',
       ];
     } else {
       this.displayedColumns = ['column1', 'column2', 'column3', 'column4'];
